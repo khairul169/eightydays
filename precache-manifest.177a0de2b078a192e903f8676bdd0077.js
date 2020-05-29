a75189203b54c9dd4eae2fb4b224093a12cce6b5
Khairul Hidayat <khairulcyber4rt@gmail.com>
2020-05-29 13:59:22 +0700

    First commit

---
+++ precache-manifest.177a0de2b078a192e903f8676bdd0077.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b54463669f200af8350a283fba115fd7",
+    "url": "/eightydays/index.html"
+  },
+  {
+    "revision": "d692e913b506a2a88605",
+    "url": "/eightydays/static/css/main.42727136.chunk.css"
+  },
+  {
+    "revision": "bb0a905cc5c1f211a141",
+    "url": "/eightydays/static/js/2.8b904852.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/eightydays/static/js/2.8b904852.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d692e913b506a2a88605",
+    "url": "/eightydays/static/js/main.3587a484.chunk.js"
+  },
+  {
+    "revision": "c9cac87ab9403b49d91a",
+    "url": "/eightydays/static/js/runtime-main.51767019.js"
+  },
+  {
+    "revision": "c51e138aaf104efb01d6ab73e3b896cd",
+    "url": "/eightydays/static/media/fb_icon.c51e138a.png"
+  },
+  {
+    "revision": "121b1f678b6de5d5979e1a5044fb6b0e",
+    "url": "/eightydays/static/media/ig_icon.121b1f67.png"
+  },
+  {
+    "revision": "ab2efe67c28a78b1499bf9f35316b4aa",
+    "url": "/eightydays/static/media/logo.ab2efe67.png"
+  },
+  {
+    "revision": "6aa621d28fd2017b57f9340bfba2850f",
+    "url": "/eightydays/static/media/map_bg.6aa621d2.png"
+  }
+]);
